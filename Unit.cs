4b7ca43b90c75e4422cccbad3471dcd01e70bbbd
Lunew <lunew12@yandex.ru>
2014-11-24 14:41:20 +0500

    Добавлен родительский класс для юнитов

---
+++ Unit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public abstract class Unit : MonoBehaviour {
+	public int XCoord;
+	public int YCoord;
+	public int Armor;
+	public int Health;
+	public int CurrentHealth;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
