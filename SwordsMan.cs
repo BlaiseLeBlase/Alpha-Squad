0e881a52a97cf4d77faad07ea54d02b50550e6d6
Lunew <lunew12@yandex.ru>
2014-11-26 10:37:43 +0500

    Изменил атаку мечников

--- SwordsMan.cs
+++ SwordsMan.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public abstract class SwordsMan : Unit {
 	private static int atackRange = 150;
-	private int Atack;
+	private static int Atack = 100;
 
 	// Use this for initialization
 	void Start () {
