eebd2c8286a54faab7435da0dfc67a79c6dc1376
Lunew <lunew12@yandex.ru>
2014-12-06 12:58:10 +0500

    Добавил всем юнитам проверку на выделение мышью и скорость передвижения

--- Unit.cs
+++ Unit.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using System.Collections;
 
-public abstract class Unit : MonoBehaviour {
-	public int XCoord;
-	public int YCoord;
-	public int Armor;
-	public int Health;
-	public int CurrentHealth;
+public  class Unit : MonoBehaviour {
+	public float XCoord;		//Координата X
+	public float YCoord;		//Координата Y
+	public int Armor;		//Количество Брони
+	public int Health;		//Максимальное количество жизней
+	public int CurrentHealth;		//Текущее количество жизней
+	public bool IsSelected;		//Выделение юнита для действий
+	public float Speed; //Скорость передвижения
+
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		}
 }
