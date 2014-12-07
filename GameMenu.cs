ef0b54731f5b81f39fc6d37a8cba6a2a5968b20b
Алексей Порозов <yageroy@mail.ru>
2014-12-07 18:38:20 +0500

    добавлены кнопки

--- GameMenu.cs
+++ GameMenu.cs
@@ -19,7 +19,17 @@ public class GameMenu : MonoBehaviour
 	{
 		if (pause) 
 		{
-		
+			GUI.depth = numDepth;
+			GUI.skin = mainUI;
+			GUI.Box(new Rect(0,0, Screen.width, Screen.height), "", GUI.Skin.GetStyle("BackgroundMenu"));
+
+			GUI.BeginGroup(new Rect((Screen.width = 150) / 2, (Screen.height = 150) / 2, 150, 150));
+
+			GUI.Label(new Rect(25, 30, 100, 30), "Pause", GUI.skin.label);
+			if (GUI.Button(new Rect(0, 50, 150, 30), "Continue"))
+			    {
+					pause = false;
+				}
 
 		}
 
